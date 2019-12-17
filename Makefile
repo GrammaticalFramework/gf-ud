9314edad0a941db77f88068f5449f257a640c6c0
Aarne Ranta <aarne@chalmers.se>
2019-12-17 07:23:32 +0100

    made minitest parametric

--- Makefile
+++ Makefile
@@ -1,8 +1,6 @@
 compile:
 	ghc -O3 --make -o gfud MainUDGF.hs
 
-# this presupposes setting the scene as described in doc/training-and-testing.txt
-minitest:
-	source minitest.sh
+
 
 
--- minitest.sh
+++ minitest.sh
@@ -1,20 +1,30 @@
-echo "generating GF trees"
+#!/bin/bash
+
+echo "generating GF trees from $1 $2 $3"
 echo "==================="
-echo "gr -cat=Utt -number=400 -depth=8 | wf -file=out/minilang.gft" | gf -run grammars/MiniLang.pgf
-head -700 out/minilang.gft | sort -u >out/minilang-train.gft
-tail -99  out/minilang.gft | sort -u >out/minilang-test.gft
+echo "gr -cat=Utt -number=400 -depth=8 | wf -file=out/$1.gft" | gf -run grammars/$1.pgf
+head -700 out/$1.gft | sort -u >out/$1-train.gft
+tail -99  out/$1.gft | sort -u >out/$1-test.gft
 echo "==================="
 echo "running gf2ud, generating conllu files"
 echo "==================="
-cat out/minilang-train.gft | ./gfud -gf2ud grammars/MiniLang Eng Utt ud | sed '/./,$!d' >out/minilang-train.conllu
-cat out/minilang-test.gft  | ./gfud -gf2ud grammars/MiniLang Eng Utt ud | sed '/./,$!d' >out/minilang-test.conllu
-#echo "==================="
-#echo "running ud2gf for comparison"
-#cat out/minilang-test.conllu | ./gfud -ud2gf grammars/MiniLang Eng Utt at >out/minilang-test-ud2gf.gft
-#cat out/minilang-test.conllu | ./gfud -ud2gf grammars/MiniLang Eng Utt stat
-#diff -y out/minilang-test.gft out/minilang-test-ud2gf.gft
-echo "==================="
-echo "training and testing Malt parser"
-echo "==================="
-java -jar maltparser.jar -c test -i out/minilang-train.conllu -m learn
-java -jar maltparser.jar -c test -i out/minilang-test.conllu -o minilang-out.conllu -m parse
+cat out/$1-train.gft | ./gfud -gf2ud grammars/$1 $2 $3 ud | sed '/./,$!d' >out/$1-train.conllu
+cat out/$1-test.gft  | ./gfud -gf2ud grammars/$1 $2 $3 ud | sed '/./,$!d' >out/$1-test.conllu
+
+if [ $4 = ud2gf ]
+then
+  echo "==================="
+  echo "running ud2gf for comparison"
+  cat out/$1-test.conllu | ./gfud -ud2gf grammars/$1 Eng Utt at >out/$1-test-ud2gf.gft
+  cat out/$1-test.conllu | ./gfud -ud2gf grammars/$1 Eng Utt stat
+  diff -y out/$1-test.gft out/$1-test-ud2gf.gft
+fi
+
+if [ $5 = malt ]
+then
+  echo "==================="
+  echo "training and testing Malt parser"
+  echo "==================="
+  java -jar maltparser.jar -c test -i out/$1-train.conllu -m learn
+  java -jar maltparser.jar -c test -i out/$1-test.conllu -o $1-out.conllu -m parse
+fi
