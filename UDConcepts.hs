148f293edf9a62820242e91ce15e53cedb6bdb6b
Arianna Masciolini <uzkamascio@gmail.com>
2021-01-06 17:22:15 +0100

    bho

--- UDConcepts.hs
+++ UDConcepts.hs
@@ -5,8 +5,8 @@ module UDConcepts where
 -- AR 2019-11-14 implementing
 -- https://universaldependencies.org/format.html
 
-import RTree
-import UDStandard
+import Gfud.RTree
+import Gfud.UDStandard
 
 import qualified Data.Set as S
 import qualified Data.Map as M
