a77ddad2fcecfebaf540881c98a12c41135e165f
Arianna Masciolini <uzkamascio@gmail.com>
2021-01-15 21:44:09 +0100

    provisroy refactor of similarity operator(s)

--- UDConcepts.hs
+++ UDConcepts.hs
@@ -98,7 +98,6 @@ class UDObject a where
   prss :: [String] -> a -- parse from separate lines
   errors :: a -> [String]  -- error messages
   check  :: a -> Either a [String]  -- return a or the error messages 
-  (~=) :: a -> a -> Bool
   prss ss = prs (unlines ss)
   prs s = prss [s]
   errors _ = []
@@ -109,7 +108,6 @@ instance UDObject UDSentence where
   prss ss = case span ((=="#") . take 1) ss of
     (cs,ws) -> UDSentence cs (map (prs . strip) ws)
   errors s = checkUDWords (udWordLines s)
-  (~=) = undefined 
 
 instance UDObject UDWord where
   prt w@(UDWord id fo le up xp fe he de ds mi) =
@@ -141,14 +139,12 @@ instance UDObject UDId where
       (a,'-':b@(_:_)) | all isDigit (a++b) -> UDIdRange (read a) (read b)
       (a,'.':b@(_:_)) | all isDigit (a++b) -> UDIdEmpty (read s)
       _ -> error ("ERROR:" ++ s ++ " invalid UDId")
-  (~=) = undefined 
 
 instance UDObject UDData where
   prt d = udArg d ++ "=" ++ concat (intersperse "," (udVals d))
   prs s = case break (=='=') (strip s) of
     (a,_:vs@(_:_)) -> UDData a (getSeps ',' vs)
     _ -> error ("ERROR:" ++ s ++ " invalid UDData")
-  (~=) = undefined 
 
 --- this works only for | separated lists...
 instance UDObject d => UDObject [d] where
@@ -159,7 +155,6 @@ instance UDObject d => UDObject [d] where
     "_" -> []
     _ -> map (prs . strip) (getSeps '|' s)
   errors ds = concatMap errors ds
-  (~=) = undefined 
 
 -- printing for Malt parser requires the metadata
 -- # sent_id = gfud1000001
