394f6bf2b996e6a87b3a3121f99e2cc0a2d07c42
aarneranta <aarne@chalmers.se>
2021-02-24 20:23:47 +0100

    experiments with Spa with more morpho tags

--- spa-test.sh
+++ spa-test.sh
@@ -1,15 +1,18 @@
+NUMBER=3200
+THRESHOLD=60
+
 cd grammars
 gf -make StructuresSpa.gf
-echo "gr -probs=Structures.probs -number=2000 -cat=Top -depth=12 | wf -file=\"../out/1-structures-spa.gft\"" | gf -run Structures.pgf
+echo "gr -probs=Structures.probs -number=$NUMBER -cat=Top -depth=12 | wf -file=\"../out/1-structures-spa.gft\"" | gf -run Structures.pgf
 sort -u ../out/1-structures-spa.gft >../out/2-structures-spa.gft
 echo "created out/2-structures-spa.gft"
 cd ..
 cat out/2-structures-spa.gft | gfud -gf2ud grammars/Structures Spa Top ud >out/1-structures-spa.conllu
 echo "created out/1-structures-spa.conllu"
-gfud cosine-similarity-sort es_gsd-ud-train.conllu out/1-structures-spa.conllu -threshold 32 SUBTREETYPE >out/2-structures-spa.conllu
+gfud cosine-similarity-sort es_gsd-ud-train.conllu out/1-structures-spa.conllu -threshold $THRESHOLD DEPREL >out/2-structures-spa.conllu
 ##mv out/1-structures-spa.conllu out/2-structures-spa.conllu
 echo "created out/2-structures-spa.conllu"
-cat out/2-structures-spa.conllu | gfud conll2pdf
+## cat out/2-structures-spa.conllu | gfud conll2pdf
 cat out/2-structures-spa.conllu | gfud statistics DEPREL
 echo "POS not covered:"
 gfud not-covered es_gsd-ud-test.conllu out/2-structures-spa.conllu POS
