9582801e5fc5f43cbf5604569fa5ed043ef99c57
Arianna Masciolini <uzkamascio@gmail.com>
2021-03-26 10:36:10 +0100

    show ud ids as ints in simplified format

--- UDConcepts.hs
+++ UDConcepts.hs
@@ -174,11 +174,11 @@ prQuickUDSentence :: UDSentence -> String
 prQuickUDSentence s = 
   intercalate "\n" (map prQuickUDWord (udWordLines s))
   where prQuickUDWord w = intercalate "\t" [
-          show $ udID w, 
+          show $ udid2int $ udID w, 
           udFORM w, 
           udLEMMA w, 
           udUPOS w, 
-          show $ udHEAD w, 
+          show $ udid2int $ udHEAD w, 
           udDEPREL w]
 
 -- example input: "1 John John NOUN 2 nsubj ; 2 walks walk VERB 0 root"
