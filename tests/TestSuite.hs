e15b807101a81b972d42c131fb65054a723b91aa
Andreas Källberg <anka.213@gmail.com>
2022-01-25 18:58:43 +0800

    Add a test for the wordform code

--- tests/TestSuite.hs
+++ tests/TestSuite.hs
@@ -7,16 +7,36 @@ import UDAnnotations
 
 
 myUDEnv :: IO UDEnv
-myUDEnv = getEnv (path "TestOrdering") "Eng" "UDS"
+myUDEnv = getEnv (path "Test") "Eng" "UDS"
   where path x = "tests/grammars/" ++ x
 
 main :: IO ()
 main = do
   env <- myUDEnv
+  someCats <- readFile "tests/grammars/some_cats.conllu"
   hspec $ do
     describe "Prefer flat trees" $ do
-        it "should pick the flatter tree of the two alternatives" $ do
+      it "should pick the flatter tree of the two alternatives" $ do
         bestTree env theCatSleepsAlready `shouldBe` "root_nsubj_obl (UseV sleep_V) (DetCN the_Det (UseN cat_N)) already_Adv"
+    describe "Allow matching on LEMMA" $ do
+      it "should handle 'LEMMA' as a UD tag" $ do
+        -- TODO: It shouldn't be using ImpVP. Fixed by not ignoring startCat (#16).
+        -- It might also be worthwhile to prioritize smaller trees of different categories,
+        -- instead of the current behaviour of preferring categories that are earlier in alphabetic order.
+        bestTrees env someCats `shouldBe`
+          ["ImpVP (UseNP (DetCN anySg_Det (UseN cat_N)))"
+          ,"ImpVP (UseNP (DetCN anyPl_Det (UseN cat_N)))"
+          ,"ImpVP (UseNP (DetCN someSg_Det (UseN cat_N)))"
+          ,"ImpVP (UseNP (DetCN somePl_Det (UseN cat_N)))"]
+
+
+bestTrees :: UDEnv -> String -> [String]
+bestTrees env conll = map exprStr exprs
+  where
+      exprs = getExprs [] env conll
+      exprStr expr = case expr of
+        (x : _xs) -> showExpr [] x
+        _ -> "bestTree: ud2gf failed"
 
 
 bestTree :: UDEnv -> String -> String
