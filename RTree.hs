60caff891e294517c746c90371cec1a57d1aa143
Andreas Källberg <anka.213@gmail.com>
2022-04-26 10:01:22 +0800

    Clean up junk

--- RTree.hs
+++ RTree.hs
@@ -11,19 +11,9 @@ data RTree a = RTree {
 
 mapRTree :: (a -> b) -> RTree a -> RTree b
 mapRTree = fmap
--- mapRTree f (RTree c ts) = RTree (f c) (map (mapRTree f) ts)
 
 allNodesRTree :: RTree a -> [a]
 allNodesRTree = toList
--- allNodesRTree t = root t : concatMap allNodesRTree (subtrees t)
-
--- Should be equivalent to foldr from the derived Foldable instance
--- foldrRTree :: (a -> b -> b) -> b -> RTree a -> b
--- foldrRTree f z t = f (root t) (foldr (flip (foldrRTree f)) z (subtrees t))
-
--- Not sure if this is useful for anything
--- cataRTree :: (a -> [b] -> b) -> RTree a -> b
--- cataRTree f t = f (root t) (map (cataRTree f) (subtrees t))
 
 prLinesRTree :: (a -> String) -> RTree a -> String
 prLinesRTree prt = unlines . pr 0 where
@@ -56,21 +46,3 @@ childrenRTree (RTree _ ts) = ts
 
 allSubRTrees :: RTree a -> [RTree a]
 allSubRTrees t = t : concatMap allSubRTrees (childrenRTree t)
-
-{-
-Generated instance, shown with {-# OPTIONS_GHC -ddump-deriv  #-}
-
-instance Data.Foldable.Foldable RTree.RTree where
-    Data.Foldable.foldr f z (RTree.RTree a1 a2)
-      = f a1
-          ((\ b3 b4
-              -> Data.Foldable.foldr
-                   (\ b1 b2 -> Data.Foldable.foldr f b2 b1) b4 b3)
-             a2 z)
-    Data.Foldable.foldMap f (RTree.RTree a1 a2)
-      = GHC.Base.mappend
-          (f a1) (Data.Foldable.foldMap (Data.Foldable.foldMap f) a2)
-    Data.Foldable.null (RTree.RTree _ _) = GHC.Types.False
-  
-
--}
